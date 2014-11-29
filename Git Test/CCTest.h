2ba18c66a8a50984d8f29ab411d156b310a1d900
ssakssri <ssakssri@gmail.com>
2014-11-29 14:51:34 +0900

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by ssakssri on 2014. 11. 29..
+//  Copyright (c) 2014년 ssakssri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by ssakssri on 2014. 11. 29..
+//  Copyright (c) 2014년 ssakssri. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
